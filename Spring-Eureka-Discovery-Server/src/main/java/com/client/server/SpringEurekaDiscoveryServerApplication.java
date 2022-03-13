ba8ac30778e950ce4cacb3333aa35ff269234eab
codeclickfilm <patilsanket741@gmail.com>
2022-03-13 12:49:29 +0530

    Created Spring Eureka Discovery Server for registration of all microservices.

---
+++ Spring-Eureka-Discovery-Server/src/main/java/com/client/server/SpringEurekaDiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package com.client.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringEurekaDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaDiscoveryServerApplication.class, args);
+	}
+
+}
